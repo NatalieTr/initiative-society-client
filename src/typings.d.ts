b58d610ff60d02402aba7c0422a76455675ed772
ZitRos <zitros.lab@gmail.com>
2017-12-28 14:26:14 +0200

    [Fix] Fix Node modules undefined typings

--- src/typings.d.ts
+++ src/typings.d.ts
@@ -1,3 +1,25 @@
 /* SystemJS module definition */
+
+interface NodeRequireFunction {
+  (id: string): any;
+}
+
+interface NodeModule {
+  exports: any;
+  require: NodeRequireFunction;
+  id: string;
+  filename: string;
+  loaded: boolean;
+  parent: NodeModule | null;
+  children: NodeModule[];
+}
+
+interface NodeRequire extends NodeRequireFunction {
+  resolve(id: string): string;
+  cache: any;
+  extensions: any;
+  main: NodeModule | undefined;
+}
+
 declare var module: NodeModule;
 declare var require: NodeRequire;

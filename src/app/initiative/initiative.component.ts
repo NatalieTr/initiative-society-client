c46b8601b8e93850bbe63b0b18d18d38b047474d
ZitRos <zitros.lab@gmail.com>
2018-01-09 11:34:05 +0200

    [Enh] Better initiative page

--- src/app/initiative/initiative.component.ts
+++ src/app/initiative/initiative.component.ts
@@ -14,6 +14,8 @@ export class InitiativeComponent implements OnInit {
   initiative: any = {};
   private sub: any;
   Math: any;
+  myWallet: string; // my wallet
+  walletCallback: any;
 
   constructor (private _initiativeService: InitiativesService, private route: ActivatedRoute) {
     this.Math = Math;
@@ -23,20 +25,43 @@ export class InitiativeComponent implements OnInit {
     return Math.round((wei || 0) / Math.pow(10, 15)) / 1000;
   }
 
+  countVotes (votes = [], positive = true) {
+    if ((votes || []).length === 0)
+      return 0;
+    return (votes || []).filter(a => positive ? a : !a).length / (votes || []).length;
+  }
+
   ngOnInit () {
     this.sub = this.route.params.subscribe(params => {
       this.id = +params['id'];
       this.loadInitiative(this.id);
     });
+    this._initiativeService.onWalletSelect(
+      this.walletCallback = wallet => this.myWallet = wallet
+    );
   }
 
   ngOnDestroy () {
     this.sub.unsubscribe();
+    this._initiativeService.releaseWalletSelect(this.walletCallback);
   }
 
   async loadInitiative (id = 1) {
     this.initiative = await this._initiativeService.getInitiativeById(id || 1);
     this.initiative.description = marked(this.initiative.description);
+    this.initiative.voting = {};
+    for (let i = 0; i < this.initiative.voters.length; ++i) {
+      this.initiative.voting[this.initiative.voters[i]] = this.initiative.votes[i];
+    }
+  }
+
+  async voteForInitiative (id = 1, positive = true) {
+
+  }
+
+  async fundInitiative (id = 1) {
+    const value = (<HTMLInputElement>document.getElementById("fund-amount")).value;
+
   }
 
 }
